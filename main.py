17a7b76d6fa4199561aa71abb5897582d6cce78e
Dhjali <barbara.vanni@laplateforme.io>
2023-11-28 15:20:06 +0100

    trying to pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import keyboard
 
 
 def afficher_heure(heure, format_12h=False):
@@ -13,12 +14,22 @@ def regler_alarme(heures, minutes, secondes):
     alarme = (heures, minutes, secondes)
     return alarme
 
+# def pause():
+#     if keyboard.is_pressed("!"):
+#         print("You pressed escape")
+
+def pause():
+    return keyboard.on_press_key("!")
+
+
+
+
 # ligne pour régler l'heure de l'alarme
 alarme_set = regler_alarme(15,5,0)
 
 
 def main():
-    format_12h = input("Utiliser le format 12h ? (oui/non) : ")
+    format_heure = input("Utiliser le format 12h ? (oui/non) : ")
     heures = int(input("Entrez l'heure de départ (hh) : "))
     minutes = int(input("Entrez les minutes de départ (mm) : "))
     secondes = int(input("Entrez les secondes de départ (ss) : "))
@@ -27,28 +38,25 @@ def main():
 
     try:
         while True:
+            if not pause():
+                if format_heure == "oui" or format_heure == "Oui":
+                    afficher_heure(heure_actuelle, True)
+                else:
+                    afficher_heure(heure_actuelle, False)
 
+                time.sleep(1)
 
-            if format_12h == "oui" or "Oui":
-                afficher_heure(heure_actuelle, True)
-            else:
-                afficher_heure(heure_actuelle)
-
-            time.sleep(1)
-
-            
-
-            heure_actuelle = (heure_actuelle[0], heure_actuelle[1], heure_actuelle[2] + 1)
-            if heure_actuelle[2] >= 60:
-                heure_actuelle = (heure_actuelle[0], heure_actuelle[1] + 1, 0)
-                if heure_actuelle[1] >= 60:
-                    heure_actuelle = (heure_actuelle[0] + 1, 0, 0)
-                    if heure_actuelle[0] >= 24:
-                        heure_actuelle = (0,0,0) 
-            
-            if heure_actuelle == alarme_set:
+                heure_actuelle = (heure_actuelle[0], heure_actuelle[1], heure_actuelle[2] + 1)
+                if heure_actuelle[2] >= 60:
+                    heure_actuelle = (heure_actuelle[0], heure_actuelle[1] + 1, 0)
+                    if heure_actuelle[1] >= 60:
+                        heure_actuelle = (heure_actuelle[0] + 1, 0, 0)
+                        if heure_actuelle[0] >= 24:
+                            heure_actuelle = (0,0,0) 
                 
-                print(*heure_actuelle,"It's time. Wake up !")
+                if heure_actuelle == alarme_set:
+                    
+                    print(*heure_actuelle,"It's time. Wake up !")
 
     except KeyboardInterrupt:
         print("\nProgramme arrêté.")
